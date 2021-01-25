3f5b9f84e8761a4c562eb93c675bb0b413a91825
javaComSci <iramanat@purdue.edu>
2021-01-24 20:04:15 -0500

    game move same pieces

--- Manchala/Manchala.py
+++ Manchala/Manchala.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import copy
 import pygame
 from random import randint
 from Player import Player, Random_Player
@@ -14,6 +15,10 @@ screen_width = 500
 screen_height = 400
 piece_width = 10
 piece_height = 10
+pieces_per_location = {}
+for i in range(2):
+    for j in range(7):
+        pieces_per_location[(i, j)] = []
 
 
 def play_game(players):
@@ -47,7 +52,7 @@ def fill_board_ui(pygame, screen):
         for j in range(2):
             pygame.draw.ellipse(screen, cup_color, (width_spacing + ellipse_width_block + width_spacing + (i * (width_spacing + ellipse_width_block)), height_spacing + (j * (2*height_spacing + ellipse_height_block)), ellipse_width_block, ellipse_height_block))
 
-def convert_start_point(screen, row, col):
+def convert_start_point(row, col):
     height_start = None
     width_start = None
     if row == 0 and col == 0:
@@ -65,23 +70,74 @@ def convert_start_point(screen, row, col):
     return (height_start, width_start)
 
 def fill_pieces_ui(pygame, screen, row, col, pieces):
-    (height_start, width_start) = convert_start_point(screen, row, col)
+    (height_start, width_start) = convert_start_point(row, col)
     height_end = height_start + (ellipse_height_block * 3 if (row == 0 and col == 0) or (row == 1 and col == 6) else ellipse_height_block)
     width_end = width_start + ellipse_width_block
     for piece in range(pieces):
         randheight = randint(height_start + 25, height_end - 25)
         randwidth = randint(width_start + 15, width_end - 15)
         piece_color = list(np.random.choice(range(256), size=3))
-        pygame.draw.ellipse(screen, piece_color, (randwidth, randheight, piece_width, piece_height))
+        ellipse = (piece_color, (randwidth, randheight, piece_width, piece_height))
+        pygame.draw.ellipse(screen, ellipse[0], ellipse[1])
+        pieces_per_location[(row, col)].append(ellipse)
 
-def fill_cup_ui(pygame, screen, board):
+def print_pieces():
+    p_0 = []
+    p_1 = []
+    for key in pieces_per_location.keys():
+        if key[0] == 0:
+            p_0.append(len(pieces_per_location[key]))
+        else:
+            p_1.append(len(pieces_per_location[key]))
+    print(p_0)
+    print(p_1)
+
+def fix_location(piece_removed, row, col):
+    (height_start, width_start) = convert_start_point(row, col)
+    height_end = height_start + (ellipse_height_block * 3 if (row == 0 and col == 0) or (row == 1 and col == 6) else ellipse_height_block)
+    width_end = width_start + ellipse_width_block
+    randheight = randint(height_start + 25, height_end - 25)
+    randwidth = randint(width_start + 15, width_end - 15)
+    piece_removed = (piece_removed[0], (randwidth, randheight, piece_width, piece_height))
+    return piece_removed
+
+def fill_cup_ui(pygame, screen, board, old_board, start_row, start_col):
     piece_width = 10
     piece_height = 10
-    for row in range(2):
-        for col in range(7):
-            if board.cups[row][col].pieces != 0:
-                fill_pieces_ui(pygame, screen, row, col, board.cups[row][col].pieces)
-    
+    if old_board == None:
+        for row in range(2):
+            for col in range(7):
+                if board.cups[row][col].pieces != 0:
+                    fill_pieces_ui(pygame, screen, row, col, board.cups[row][col].pieces)
+    else:
+        print("BEFORE!---------------------------------------------------", start_row, start_col, old_board.cups[start_row][start_col].pieces, board.cups[start_row][start_col].pieces )
+        print_pieces()
+        old_board.print_board()
+        board.print_board()
+        for row in range(2):
+            for col in range(7):
+                if old_board.cups[row][col].pieces != board.cups[row][col].pieces:
+                    if (start_row == row and start_col == col) == False:
+                        num_movement = board.cups[row][col].pieces - old_board.cups[row][col].pieces
+                        # print("AT ", row, col, num_movement)
+                        moved = 0
+                        while moved < num_movement:
+                            # old board had less pieces so need to move to here from starting position
+                            piece_removed = pieces_per_location[(start_row, start_col)].pop(0)
+                            print(piece_removed)
+                            piece_removed = fix_location(piece_removed, row, col)
+                            pieces_per_location[(row, col)].append(piece_removed)
+                            print(piece_removed)
+                            # print(pieces_per_location[(start_row, start_col)], pieces_per_location[(row, col)])
+                            moved += 1
+        for row in range(2):
+            for col in range(7):
+                for piece in pieces_per_location[(row, col)]:
+                    pygame.draw.ellipse(screen, piece[0], piece[1])
+        print_pieces()
+        print("AFTER!---------------------------------------------------")
+
+
 def play_game_with_ui(players):
     board = Board()
     print("---Starting game---")
@@ -90,6 +146,8 @@ def play_game_with_ui(players):
     pygame.init()
     screen = pygame.display.set_mode([screen_width, screen_height])
     pygame.display.flip()
+    fill_board_ui(pygame, screen)
+    fill_cup_ui(pygame, screen, board, None, None, None)
 
     turn = 0
     running = True
@@ -101,10 +159,11 @@ def play_game_with_ui(players):
         while board.game_end() == False:
             again = True
             while again == True and board.game_end() == False:
+                old_board = copy.deepcopy(board)
                 index = players[turn].pick_move(board)
                 (board, again) = players[turn].move(board, index)
                 fill_board_ui(pygame, screen)
-                fill_cup_ui(pygame, screen, board)
+                fill_cup_ui(pygame, screen, board, old_board, turn, index)
                 pygame.display.flip()
                 pygame.time.delay(300) 
             turn = (turn + 1) % 2
